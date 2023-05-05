33d0cd8641d44655a6645fd8def203519f10f013
Ryanangry07 <S2022048@siswa.um.edu.my>
2023-05-05 11:35:04 +0800

    Email verification
    Added the email field in register form and password reset form. Changed the username field to email in login form.
    
    System will alert user when user submit the password reset form.

--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,18 @@
 const loginForm = document.querySelector('#login-form');
-const usernameInput = loginForm.querySelector('input[name="username"]');
+//const usernameInput = loginForm.querySelector('input[name="username"]');
 const passwordInput = loginForm.querySelector('input[name="password"]');
-const confirmPasswordInput = registerForm.querySelector('input[name="confirm-password"]');
+const confirmPasswordInput = loginForm.querySelector('input[name="confirm-password"]');
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   // Username constraints
-  if (usernameInput.value.length < 3) {
-    alert('Username must be at least 3 characters long');
-    return;
-  }
+  // if (usernameInput.value.length < 3) {
+  //   alert('Username must be at least 3 characters long');
+  //   return;
+  // }
+
+
 
   // Password constraints
   if (passwordInput.value.length < 8) {
